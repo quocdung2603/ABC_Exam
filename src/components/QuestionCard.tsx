c6d2e67c9c90d66fc0efad5a41afad703e3be2d5
Dũng Nguyễn Quốc <nguyenquocdung26032003@gmail.com>
2024-12-22 21:57:32 +0700

    3

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -8,14 +8,14 @@ interface QuestionCardProps {
 
 const QuestionCard: React.FC<QuestionCardProps> = ({ question, options, onAnswer }) => {
   return (
-    <div className="bg-white shadow-md rounded-lg p-6">
-      <h3 className="text-lg font-bold mb-4">{question}</h3>
+    <div className="bg-white shadow-md rounded-lg p-6 w-full">
+      <h3 className="text-sm text-justify font-bold mb-4">{question}</h3>
       <ul className="grid grid-cols-1 gap-2 sm:grid-cols-2">
         {options.map((option, index) => (
           <li key={index+0}>
             <button
               onClick={() => onAnswer(option)}
-              className="w-full bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md"
+              className="w-full bg-blue-500 hover:bg-blue-600 text-sm text-justify text-white py-2 px-4 rounded-md"
             >
               {option}
             </button>
--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { shuffleArray } from '../utils/shuffle';
 import QuestionCard from '../components/QuestionCard';
-import { SampleData } from '../fakeData/data';
 
 interface Question {
   id: number;
@@ -18,12 +18,12 @@ const QuizPage: React.FC<{ onComplete: (score: number, total: number) => void }>
   const [score, setScore] = useState<number>(0);
   const [answeredQuestions, setAnsweredQuestions] = useState<number[]>([]);
   const [flaggedQuestions, setFlaggedQuestions] = useState<number[]>([]); // Trạng thái đánh dấu
-  const [showQuestionList, setShowQuestionList] = useState<boolean>(false); // Hiển thị danh sách câu hỏi
 
   useEffect(() => {
-    const shuffledQuestions = shuffleArray(SampleData);
-    console.log(shuffledQuestions);
-    setQuestions(shuffledQuestions);
+    axios.get('http://localhost:3001/questions').then((response) => {
+      const shuffledQuestions = shuffleArray(response.data);
+      setQuestions(shuffledQuestions);
+    });
   }, []);
 
   const handleAnswer = (selectedAnswer: string) => {
@@ -47,22 +47,78 @@ const QuizPage: React.FC<{ onComplete: (score: number, total: number) => void }>
     onComplete(score, questions.length);
   };
 
+  const goToNextQuestion = () => {
+    if (currentQuestionIndex < questions.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    }
+  };
+
+  const goToPreviousQuestion = () => {
+    if (currentQuestionIndex > 0) {
+      setCurrentQuestionIndex(currentQuestionIndex - 1);
+    }
+  };
+
   return (
     <div className="flex flex-col md:flex-row h-screen">
+      {/* Nội dung chi tiết câu hỏi */}
+      <div className="flex-1 p-4">
+        {questions.length > 0 && (
+          <>
+            <QuestionCard
+              question={questions[currentQuestionIndex].question}
+              options={questions[currentQuestionIndex].options}
+              onAnswer={(answer) => handleAnswer(answer)}
+            />
+            <div className='flex flex-row w-full justify-between items-center mt-4'>
+              <div className='mr-auto'>
+                <button
+                  onClick={() => toggleFlag(currentQuestionIndex)}
+                  className={`px-4 py-2 rounded-md ${flaggedQuestions.includes(currentQuestionIndex)
+                    ? 'bg-yellow-500 text-white'
+                    : 'bg-gray-300 text-black'
+                    }`}
+                >
+                  {flaggedQuestions.includes(currentQuestionIndex)
+                    ? 'Bỏ Cờ'
+                    : 'Đặt Cờ'}
+                </button>
+              </div>
+              <div className='flex flex-row ml-auto space-x-4'>
+                <button
+                  onClick={goToPreviousQuestion}
+                  className="px-4 py-2 bg-gray-300 rounded-md text-black disabled:opacity-50"
+                  disabled={currentQuestionIndex === 0}
+                >
+                  Trước
+                </button>
+                <button
+                  onClick={goToNextQuestion}
+                  className="px-4 py-2 bg-gray-300 rounded-md text-black disabled:opacity-50"
+                  disabled={currentQuestionIndex === questions.length - 1}
+                >
+                  Tiếp
+                </button>
+              </div>
+            </div>
+            <button
+              onClick={handleFinish}
+              className="mt-4 w-full bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded"
+            >
+              Kết thúc làm bài
+            </button>
+          </>
+        )}
+      </div>
+
       {/* Danh sách câu hỏi */}
-      <div
-        className={`${showQuestionList ? 'block' : 'hidden'
-          } md:block w-full md:w-1/3 bg-gray-100 overflow-y-auto p-4`}
-      >
+      <div className="w-full md:w-1/3 bg-gray-100 overflow-y-auto p-4">
         <h2 className="text-xl font-bold mb-4">Danh sách câu hỏi</h2>
         <div className="grid grid-cols-5 gap-2">
           {questions.map((_, index) => (
             <button
-              key={index+0}
-              onClick={() => {
-                setCurrentQuestionIndex(index);
-                setShowQuestionList(false); // Tự động đóng danh sách ở màn hình nhỏ
-              }}
+              key={index + 0}
+              onClick={() => setCurrentQuestionIndex(index)}
               className={`p-2 rounded ${answeredQuestions.includes(index) ? 'bg-green-400' : 'bg-gray-200'
                 } ${flaggedQuestions.includes(index) ? 'ring-2 ring-yellow-500' : ''
                 } ${currentQuestionIndex === index ? 'ring-2 ring-blue-500' : ''
@@ -73,45 +129,6 @@ const QuizPage: React.FC<{ onComplete: (score: number, total: number) => void }>
           ))}
         </div>
       </div>
-
-      {/* Nội dung chi tiết câu hỏi */}
-      <div className="flex-1 p-4">
-        {questions.length > 0 && (
-          <>
-            <QuestionCard
-              question={questions[currentQuestionIndex].question}
-              options={questions[currentQuestionIndex].options}
-              onAnswer={(answer) => handleAnswer(answer)}
-            />
-            <button
-              onClick={() => toggleFlag(currentQuestionIndex)}
-              className={`mt-4 px-4 py-2 rounded ${flaggedQuestions.includes(currentQuestionIndex)
-                ? 'bg-yellow-500 text-white'
-                : 'bg-gray-300 text-black'
-                }`}
-            >
-              {flaggedQuestions.includes(currentQuestionIndex)
-                ? 'Bỏ đánh dấu'
-                : 'Đặt cờ'}
-            </button>
-          </>
-        )}
-
-        <button
-          onClick={handleFinish}
-          className="mt-4 w-full bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded"
-        >
-          Kết thúc làm bài
-        </button>
-      </div>
-
-      {/* Nút mở danh sách câu hỏi trên màn hình nhỏ */}
-      <button
-        className="block md:hidden bg-blue-500 text-white fixed bottom-4 right-4 rounded-full w-14 h-14"
-        onClick={() => setShowQuestionList(!showQuestionList)}
-      >
-        📋
-      </button>
     </div>
   );
 };
